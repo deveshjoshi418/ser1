74ae721de6a21de0958b7bc02a6dcad0f0d06bd0
deveshjoshi418 <63827215+deveshjoshi418@users.noreply.github.com>
2025-02-20 18:01:45 -0500

    Update ser1.py

--- ser1.py
+++ ser1.py
@@ -16,6 +16,6 @@ with col1:
                      # url="https://1drv.ms/x/c/8de00afca386d6d5/EW1g77HDZydJic0Vet5QXaYB0kRlqipf75t6WhQ9VdxRfA?e=FutMWU")
                      url="https://onedrive.live.com/personal/8de00afca386d6d5/_layouts/15/doc.aspx?resid=b1ef606d-67c3-4927-89cd-157ade505da6&cid=8de00afca386d6d5&ct=1740085175166&wdOrigin=OFFICECOM-WEB.START.EDGEWORTH&wdPreviousSessionSrc=HarmonyWeb&wdPreviousSession=db424c9f-d3c3-4105-baac-667541cc50cf")
     st.link_button(label="Ser1",
-                  url="https://onedrive.live.com/personal/8de00afca386d6d5/_layouts/15/doc.aspx?resid=b1ef606d-67c3-4927-89cd-157ade505da6&cid=8de00afca386d6d5&ct=1740085175166&wdOrigin=OFFICECOM-WEB.START.EDGEWORTH&wdPreviousSessionSrc=HarmonyWeb&wdPreviousSession=db424c9f-d3c3-4105-baac-667541cc50cf")
+                  url="Ser1")
 with col2:
     st.image("1.jpg", width=300)
