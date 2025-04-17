1618569444a863b25dea4a372fc46232eb325790
deveshjoshi418 <63827215+deveshjoshi418@users.noreply.github.com>
2025-04-17 10:37:58 -0400

    Update ser1.py

--- ser1.py
+++ ser1.py
@@ -24,15 +24,15 @@ with col2:
     st.image("1.jpg", width=300)
 
 sheet_id = "1Roe8IzbNcUi4rwRpYZQb4Zvmpqvi-iDTvRbwZSTRnkY"
-    gid = "0"  # GID of the first sheet
+gid = "0"  # GID of the first sheet
 
-    csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
+csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
 
-    # Load the CSV into a DataFrame
-    df = pd.read_csv(csv_url)
-    dfst = pd.DataFrame(df, index=["row1"])
+# Load the CSV into a DataFrame
+df = pd.read_csv(csv_url)
+dfst = pd.DataFrame(df, index=["row1"])
 
-    st.dataframe(df, hide_index=True)
+st.dataframe(df, hide_index=True)
     
     
 
