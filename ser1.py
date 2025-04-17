eda5c08c38ee0a9922b0f3062b6c9d0fe9a69f88
deveshjoshi418 <63827215+deveshjoshi418@users.noreply.github.com>
2025-04-17 13:56:46 -0400

    Update ser1.py

--- ser1.py
+++ ser1.py
@@ -24,7 +24,7 @@ with col3:
     csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
 
     df = pd.read_csv(csv_url)
-    dfst = pd.DataFrame(df, index=(0:100), columns=["Name"])
+    dfst = pd.DataFrame(df, index=[0,1,2,3,4,5,6,7], columns=["Name"])
 
     st.dataframe(dfst, width= 515, hide_index=True, use_container_width=False)
     
