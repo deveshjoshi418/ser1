0f26a779d0a6d5de1e2201ff23203207bae5b60f
deveshjoshi418 <63827215+deveshjoshi418@users.noreply.github.com>
2025-04-17 12:06:54 -0400

    Update ser1.py

--- ser1.py
+++ ser1.py
@@ -32,7 +32,7 @@ with col3:
     df = pd.read_csv(csv_url)
     dfst = pd.DataFrame(df, index=["row1"])
 
-    st.dataframe(df, width= 510, hide_index=True, use_container_width=False)
+    st.dataframe(df, width= 515, hide_index=True, use_container_width=False)
     
     
 
